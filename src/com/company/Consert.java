3fadc393db82a0e175208cd4d5ef5d7b9d9b7ee4
Andreas Sanz Lerena <94andsan@mk.se>
2018-12-06 11:49:56 +0100

    Fix

---
+++ src/com/company/Consert.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Consert {
+    private String name;
+    private int price;
+
+    public Consert(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,74 @@
+package com.company;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class Main extends Application {
+
+    ArrayList<Consert> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) {
+        ObservableList<String> options =
+                FXCollections.observableArrayList(
+                        "1",
+                        "2",
+                        "3"
+                );
+        final ComboBox comboBox = new ComboBox(options);
+        comboBox.getSelectionModel().selectFirst();
+
+        Text text = new Text("");
+
+        Button btn = new Button();
+        btn.setText("Lägg till");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                switch (comboBox.getValue().toString()) {
+                    case "1":
+                        list.add(new Consert("Konsert 1", 5000));
+                }
+                updateListText(text);
+            }
+        });
+
+        Separator separator1 = new Separator();
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+
+        grid.add(comboBox, 0, 0);
+        grid.add(btn, 1, 0);
+        grid.add(separator1, 0, 1);
+        grid.add(text, 0, 2);
+
+        stage.setScene(new Scene(grid, 400, 300));
+        stage.show();
+    }
+
+    private void updateListText(Text text) {
+        String s = "";
+        for (Consert list : list) {
+            s += list.getName() + " - " + list.getPrice() + "kr." + "\n";
+        }
+        text.setText(s);
+    }
+}
