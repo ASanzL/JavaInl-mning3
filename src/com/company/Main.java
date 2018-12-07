e103e4ce29663dec067a8fab2e0bac6a7266fc79
Andreas Sanz <andreas.sanz@hotmail.com>
2018-12-07 16:51:41 +0100

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,8 +22,11 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage stage) {
+        stage.setTitle("Boka konsert");
+
         ObservableList<String> options =
                 FXCollections.observableArrayList(
                         "1",
